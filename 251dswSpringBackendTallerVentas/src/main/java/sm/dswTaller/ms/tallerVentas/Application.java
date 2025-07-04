da5a79bffd5eb4a7bf3fbcd16fda813d2cf69f18
Lichal2108 <richard232108@gmail.com>
2025-07-04 09:53:20 -0500

    cambios de configuracion de algunos yml

--- 251dswSpringBackendTallerVentas/src/main/java/sm/dswTaller/ms/tallerVentas/Application.java
+++ 251dswSpringBackendTallerVentas/src/main/java/sm/dswTaller/ms/tallerVentas/Application.java
@@ -2,6 +2,8 @@ package sm.dswTaller.ms.tallerVentas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
